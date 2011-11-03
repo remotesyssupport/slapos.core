4960b97da7f73be631c905f0852cfbe3c90c4fbd
Romain Courteaud <romain@nexedi.com>
2011-11-03 11:28:12 +0100

    Software type is provided by the sequence

--- master/product/Vifib/tests/testVifibSlapWebService.py
+++ master/product/Vifib/tests/testVifibSlapWebService.py
@@ -3095,7 +3095,9 @@ class TestVifibSlapWebServiceMixin(testVifibMixin):
         'slap_computer_partition_id': partition_id,
         'slap_server_url': self.server_url,
         'slap_software_release_url': software_release_uri,
-        'slap_software_type': 'RootSoftwareInstance',
+        'slap_software_type': 
+          sequence.get('requested_software_type',
+                       'requested_software_type'),
         'test_parameter': 'lala',
         'slave_instance_list': [],
         'ip_list': [],
