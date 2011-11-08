d57e16a49fa6566ce08038e423940acedb93c865
Romain Courteaud <romain@nexedi.com>
2011-11-08 13:34:17 +0100

    Test skeleton.

---
+++ master/product/Vifib/tests/testVifibOpenOrderSimulation.py
@@ -0,0 +1,108 @@
+from Products.ERP5Type.tests.Sequence import SequenceList
+import unittest
+from testVifibSlapWebService import TestVifibSlapWebServiceMixin
+
+class TestVifibOpenOrderSimulation(TestVifibSlapWebServiceMixin):
+
+  def stepCheckSimulationMovement(self, sequence, **kw):
+    person = self.portal.ERP5Site_getAuthenticatedMemberPersonValue()
+    open_order = person.getDestinationDecisionRelatedValue(
+        portal_type="Open Sale Order")
+    open_order_line_list = open_order.contentValues(
+        portal_type="Open Sale Order Line")
+    self.assertEquals(1, len(open_order_line_list))
+
+    open_order_line = open_order_line_list[0]
+    hosting_subscription = open_order_line.getAggregateValue(
+      portal_type="Hosting Subscription")
+
+    applied_rule = hosting_subscription.getCausalityRelatedValue(
+        portal_type="Applied Rule")
+    self.assertEquals(
+        "portal_rules/default_subscription_item_rule", 
+        applied_rule.getSpecialise())
+
+    # Check periodicity of the open sale order line (like on alarm)
+    self.assertEquals("XXX", open_order_line.getPeriodicityMinuteFrequency())
+    self.assertEquals("XXX", open_order_line.getPeriodicityMinuteList())
+    self.assertEquals("XXX", open_order_line.getPeriodicityHourFrequency())
+    self.assertEquals("XXX", open_order_line.getPeriodicityHourList())
+    self.assertEquals("XXX", open_order_line.getPeriodicityDayFrequency())
+    self.assertEquals("XXX", open_order_line.getPeriodicityDayList())
+    self.assertEquals("XXX", open_order_line.getPeriodicityMonthFrequency())
+    self.assertEquals("XXX", open_order_line.getPeriodicityMonthList())
+    self.assertEquals("XXX", open_order_line.getPeriodicityMonthDayList())
+    self.assertEquals("XXX", open_order_line.getPeriodicityWeekFrequency())
+    self.assertEquals("XXX", open_order_line.getPeriodicityWeekList())
+    self.assertEquals("XXX", open_order_line.getPeriodicityWeekDayList())
+    
+    # Check start/stop date
+    self.assertEquals("XXX", open_order_line.getPeriodicityStopDate())
+    self.assertEquals("XXX", open_order_line.getStartDate())
+
+    # Calculate the list of time frames
+    expected_time_frame_list = []
+
+    # Check that simulation is created by the periodicity
+    self.assertEquals(len(expected_time_frame), 
+                      len(applied_rule.contentValues()))
+
+    # Check the list of expected simulation
+    for expected_time_frame in expected_time_frame_list:
+      simulation_movement = self.portal.portal_catalog.getResultValue(
+          parent_uid=applied_rule.getUid(),
+          portal_type="Simulation Movement",
+          start_date=expected_time_frame[0],
+          stop_date=expected_time_frame[1],
+          )
+      self.assertNotEquals(None, simulation_movement)
+
+      # Check simulation movement property
+      self.assertNotEquals(1, simulation_movement.getQuantity())
+      self.assertNotEquals("XXX", simulation_movement.getQuantityUnit())
+      self.assertNotEquals(1, simulation_movement.getPrice())
+      self.assertNotEquals("EUR", simulation_movement.getPriceCurrency())
+      # XXX supplier
+      self.assertNotEquals("XXX", simulation_movement.getSource())
+      self.assertNotEquals("XXX", simulation_movement.getSourceSection())
+      # XXX customer
+      self.assertNotEquals("XXX", simulation_movement.getDestination())
+      self.assertNotEquals("XXX", simulation_movement.getDestinationSection())
+
+      self.assertNotEquals("XXX", simulation_movement.getSpecialise())
+
+      self.assertNotEquals("XXX", simulation_movement.getResource())
+
+      self.assertNotEquals("XXX", 
+                           simulation_movement.getAggregate(
+                             portal_type="Computer Partition"))
+      self.assertNotEquals("XXX", 
+                           simulation_movement.getAggregate(
+                             portal_type="Software Instance"))
+      self.assertNotEquals("XXX", 
+                           simulation_movement.getAggregate(
+                             portal_type="Hosting Subscription"))
+      self.assertNotEquals("XXX", 
+                           simulation_movement.getAggregate(
+                             portal_type="Software Release"))
+
+  def test_OpenOrder_request_changeSoftwareType(self):
+    """
+    Check that requesting the same instance with a different software type
+    does not create a new instance
+    """
+    self.computer_partition_amount = 1
+    sequence_list = SequenceList()
+    sequence_string = self.prepare_person_requested_software_instance + """
+      LoginTestVifibCustomer
+      CheckSimulationMovement
+      Tic
+      SlapLogout
+    """
+    sequence_list.addSequenceString(sequence_string)
+    sequence_list.play(self)
+
+def test_suite():
+  suite = unittest.TestSuite()
+  suite.addTest(unittest.makeSuite(TestVifibOpenOrderSimulation))
+  return suite
