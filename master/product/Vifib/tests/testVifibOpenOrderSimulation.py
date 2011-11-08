99cd261b1286cccf85c3b9cc5a92ca027278d3cc
Yingjie Xu <yxu@nexedi.com>
2011-11-08 17:30:59 +0100

    Update test script.

--- master/product/Vifib/tests/testVifibOpenOrderSimulation.py
+++ master/product/Vifib/tests/testVifibOpenOrderSimulation.py
@@ -2,6 +2,8 @@ from Products.ERP5Type.tests.Sequence import SequenceList
 import unittest
 from testVifibSlapWebService import TestVifibSlapWebServiceMixin
 
+from DateTime.DateTime import DateTime
+
 class TestVifibOpenOrderSimulation(TestVifibSlapWebServiceMixin):
 
   def stepCheckSimulationMovement(self, sequence, **kw):
@@ -19,42 +21,67 @@ class TestVifibOpenOrderSimulation(TestVifibSlapWebServiceMixin):
     applied_rule = hosting_subscription.getCausalityRelatedValue(
         portal_type="Applied Rule")
     self.assertEquals(
-        "portal_rules/default_subscription_item_rule", 
+        "portal_rules/default_subscription_item_rule",
         applied_rule.getSpecialise())
 
-    # Check periodicity of the open sale order line (like on alarm)
-    self.assertEquals("XXX", open_order_line.getPeriodicityMinuteFrequency())
-    self.assertEquals("XXX", open_order_line.getPeriodicityMinuteList())
-    self.assertEquals("XXX", open_order_line.getPeriodicityHourFrequency())
-    self.assertEquals("XXX", open_order_line.getPeriodicityHourList())
-    self.assertEquals("XXX", open_order_line.getPeriodicityDayFrequency())
-    self.assertEquals("XXX", open_order_line.getPeriodicityDayList())
-    self.assertEquals("XXX", open_order_line.getPeriodicityMonthFrequency())
-    self.assertEquals("XXX", open_order_line.getPeriodicityMonthList())
-    self.assertEquals("XXX", open_order_line.getPeriodicityMonthDayList())
-    self.assertEquals("XXX", open_order_line.getPeriodicityWeekFrequency())
-    self.assertEquals("XXX", open_order_line.getPeriodicityWeekList())
-    self.assertEquals("XXX", open_order_line.getPeriodicityWeekDayList())
-    
-    # Check start/stop date
-    self.assertEquals("XXX", open_order_line.getPeriodicityStopDate())
-    self.assertEquals("XXX", open_order_line.getStartDate())
+    self.assertEquals(None, hosting_subscription.getPeriodicityMinuteFrequency())
+    self.assertEquals([0], hosting_subscription.getPeriodicityMinuteList())
+    self.assertEquals(None, hosting_subscription.getPeriodicityHourFrequency())
+    self.assertEquals([0], hosting_subscription.getPeriodicityHourList())
+    self.assertEquals(None, hosting_subscription.getPeriodicityDayFrequency())
+    self.assertEquals(None, hosting_subscription.getPeriodicityMonthFrequency())
+    self.assertEquals([1], hosting_subscription.getPeriodicityMonthDayList())
+    self.assertEquals(None, hosting_subscription.getPeriodicityWeekFrequency())
+
+    today = DateTime()
+    year = today.year()
+    month = today.month()
+    day = today.day()
+    self.assertEquals(year, open_order_line.getStartDate().year())
+    self.assertEquals(month, open_order_line.getStartDate().month())
+    self.assertEquals(day, open_order_line.getStartDate().day())
+    to_year = year + 1
+    to_month = month + 1
+    if to_month > 12:
+      to_year += 1
+      to_month -=12
+    to_day = 1
+    self.assertEquals(to_year, open_order_line.getStopDate().year())
+    self.assertEquals(to_month, open_order_line.getStopDate().month())
+    self.assertEquals(to_day, open_order_line.getStopDate().day())
 
     # Calculate the list of time frames
     expected_time_frame_list = []
+    current_year = year
+    current_month = month + 1
+    if current_month > 12:
+      current_year += 1
+      current_month -= 12
+    current_day = 1
+    current = DateTime(current_year, current_month, current_day)
+    while current <= DateTime(to_year, to_month, to_day):
+      expected_time_frame_list.append(current)
+      current_month += 1
+      if current_month > 12:
+        current_year += 1
+        current_month -= 12
+      current = DateTime(current_year, current_month, current_day)
 
     # Check that simulation is created by the periodicity
-    self.assertEquals(len(expected_time_frame), 
-                      len(applied_rule.contentValues()))
+    self.assertEquals(len(expected_time_frame_list),
+                      len(applied_rule.contentValues()) + 1)
 
     # Check the list of expected simulation
-    for expected_time_frame in expected_time_frame_list:
+    idx = 0
+    while idx + 1 < len(expected_time_frame_list):
       simulation_movement = self.portal.portal_catalog.getResultValue(
           parent_uid=applied_rule.getUid(),
           portal_type="Simulation Movement",
-          start_date=expected_time_frame[0],
-          stop_date=expected_time_frame[1],
-          )
+          **{
+            'movement.start_date':expected_time_frame_list[idx],
+            'movement.stop_date':expected_time_frame_list[idx + 1]
+          })
+      import pdb; pdb.set_trace()
       self.assertNotEquals(None, simulation_movement)
 
       # Check simulation movement property
@@ -73,16 +100,16 @@ class TestVifibOpenOrderSimulation(TestVifibSlapWebServiceMixin):
 
       self.assertNotEquals("XXX", simulation_movement.getResource())
 
-      self.assertNotEquals("XXX", 
+      self.assertNotEquals("XXX",
                            simulation_movement.getAggregate(
                              portal_type="Computer Partition"))
-      self.assertNotEquals("XXX", 
+      self.assertNotEquals("XXX",
                            simulation_movement.getAggregate(
                              portal_type="Software Instance"))
-      self.assertNotEquals("XXX", 
+      self.assertNotEquals("XXX",
                            simulation_movement.getAggregate(
                              portal_type="Hosting Subscription"))
-      self.assertNotEquals("XXX", 
+      self.assertNotEquals("XXX",
                            simulation_movement.getAggregate(
                              portal_type="Software Release"))
 
